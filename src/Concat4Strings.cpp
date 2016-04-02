561f8d3e7d7201a23e14e455135468972c1f9f81
vamsichinnu <vamsichinnu@missionrnd.com>
2016-04-02 21:13:45 +0530

    Submit Commit

--- src/Concat4Strings.cpp
+++ src/Concat4Strings.cpp
@@ -29,9 +29,30 @@ Difficulty : Hard
 
 char * concat_4strings(char *s1, char *s2, char *s3, char *s4){
 	//Return the new string created.
+	int *a1, *a2, *a3, *a4,i;
+	a1 = (int *)malloc(sizeof(int)* 26);
+	a2 = (int *)malloc(sizeof(int)* 26);
+	a3 = (int *)malloc(sizeof(int)* 26);
+	a4 = (int *)malloc(sizeof(int)* 26);
 	if (s1&&s2&&s3&&s4)
 	{
-
+		for (i = 0; s1[i] != NULL; i++)
+		{
+			a1[s1[i] - '0']++;
+		}
+		for (i = 0; s2[i] != NULL; i++)
+		{
+			a2[s2[i] - '0']++;
+		}
+		for (i = 0; s3[i] != NULL; i++)
+		{
+			a3[s3[i] - '0']++;
+		}
+		for (i = 0; s4[i] != NULL; i++)
+		{
+			a4[s4[i] - '0']++;
+		}
+		
 	}
 	else
 	{
